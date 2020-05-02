c18542428c7d08ed95a695a852302dd15c9460de
Faber <fabriziocasadei97@gmail.com>
2020-05-02 13:50:36 +0200

    analizzate alcune operazioni base

--- programming basics/PythonPath.py
+++ programming basics/PythonPath.py
@@ -1,3 +1,4 @@
 import os
 import sys
 print(os.path.dirname(sys.executable))
+exit()
--- programming basics/helloWolrd.py
+++ programming basics/helloWolrd.py
@@ -1,6 +1,9 @@
+import this
 message="hello world!"
 print(message)
-message="hello worldx2!"
+print(message.title())
+message= message.upper()
 print(message)
-
+print(message.lower())
+#this is a comment, after that, this program'll exit
 exit()
