10df517dca091f582d140c6a16f9d96bb394c560
Faber <fabriziocasadei97@gmail.com>
2020-07-01 11:48:39 +0200

    modificati i modelli e aggiunto un file con i comandi da terminale usati

--- Project 3, Getting started with Django/learning_log/learning_logs/models.py
+++ Project 3, Getting started with Django/learning_log/learning_logs/models.py
@@ -10,5 +10,16 @@ class Topic (models.Model): #sottoclasse di Model
     def __str__(self):
         return self.text
 
-    
+class Entry(models.model):
+    """ descrizione di un argomento (Topic), relazione many to one con essa """
+    topic= models.ForeignKey(Topic)  #id dei topics
+    text= models.TextField()
+    date_added=models.DateTimeField(auto_now_add=True)
+
+    """Meta contiene informazioni supplementari per la gestione dei modelli"""
+    class Meta:
+        verbose_name_plural= "entries" #entries invece di entrys
+
+        def __str__(self):
+            return self.text[:50] + "..." #ritorna i primi 50 caratteri
 
